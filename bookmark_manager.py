8d9acedb48ee29562b84d4d41391d4305519a84b
Avinash Ravilla <avinashchowdary0801@gmail.com>
2018-03-16 15:37:04 -0700

    added already processed check
    if a bookmark has already been matched with other, no need to process it
    again with some other. as we are going linearly, all other matches will
    be found in one iteration itself

--- bookmark_manager.py
+++ bookmark_manager.py
@@ -4,6 +4,8 @@ def main():
     secure = "https://"
     un_secure = "http://"
     result_bkmks = []
+    # alerady processes bookmarks are stored in this list
+    visited_bkmks = []
     
     safari_set = set(line.strip() for line in open('safari.txt'))
     chrome_set = set(line.strip() for line in open('chrome.txt'))
@@ -16,35 +18,58 @@ def main():
     total_bkmks.sort(key = lambda s: len(s))
 
     for actual in total_bkmks:
-        temp_list = []
-        a_protocol, a_link = separate_protocol_link(actual)
 
-        for repeated in total_bkmks:
-            if actual == repeated:
-                continue
-            if a_link in repeated:
-                if un_secure in a_protocol:
-                    temp_list.append(repeated)
-                    temp_list.append(actual)
+        # bookmark has already been compared with others
+        if actual in visited_bkmks:
+            pass
+        else:
+            temp_list = []
+            a_protocol, a_link = separate_protocol_link(actual)
+
+            for repeated in total_bkmks:
+
+                # bookmark has already been compared with others    
+                if repeated in visited_bkmks:
+                    pass
                 else:
-                    temp_list.append(actual)
-                    temp_list.append(repeated)
+                    # comparin with itself
+                    # do nothing
+                    if actual == repeated:
+                        continue
+                    # similar bookmark
+                    # similar web address
+                    # different end point/security
+                    if a_link in repeated:
+                        # http links at the end
+                        if un_secure in a_protocol:
+                            temp_list.append(repeated)
+                            temp_list.append(actual)
+                        else:
+                            temp_list.append(actual)
+                            temp_list.append(repeated)
         
+        # update visited bookmarks 
+        # after each iteration
+        for temp in temp_list:
+            visited_bkmks.append(temp)
+        
+        # temp list empty
+        # donot add empty to final result
         if not temp_list:
             pass
         else:
+            # processed bookmarks
+            # final result
             result_bkmks.append(temp_list)
                 
     print(result_bkmks)
 
+
 def separate_protocol_link(bkmk):
+    # splitting to get
+    # protocol and web address
     protocol, url = bkmk.split("://")
     return protocol, url
-
-
-
-    
-
 
 if __name__ == '__main__':
     main()
