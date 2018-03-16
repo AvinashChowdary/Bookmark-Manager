6cf6868cf69248627def46132702610386f24c57
Avinash Ravilla <avinashchowdary0801@gmail.com>
2018-03-16 15:08:08 -0700

    sorted the bookmarks based on size and trying to aggregate them

--- bookmark_manager.py
+++ bookmark_manager.py
@@ -2,21 +2,18 @@
 def main():
     
     safari_set = set(line.strip() for line in open('safari.txt'))
-
-    print(safari_set)
-
     chrome_set = set(line.strip() for line in open('chrome.txt'))
-
-    print(chrome_set)
-
     firefox_set = set(line.strip() for line in open('firefox.txt'))
-
-    print(firefox_set)
 
     temp_set = safari_set.union(chrome_set)
     total_set = temp_set.union(firefox_set)
 
     print(total_set)
+
+    total_bkmks = list(total_set)
+    
+
+
 
 if __name__ == '__main__':
     main()
