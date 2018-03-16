4ad4857ae5ee7bb8ab48ea0b05053fef60a5b473
Avinash Ravilla <avinashchowdary0801@gmail.com>
2018-03-16 14:53:37 -0700

    reading bookmarks as a set from files

--- bookmark_manager.py
+++ bookmark_manager.py
@@ -1,6 +1,19 @@
 
 def main():
-    //begin
+    
+    safari_set = set(line.strip() for line in open('safari.txt'))
+
+    print(safari_set)
+
+    chrome_set = set(line.strip() for line in open('chrome.txt'))
+
+    print(chrome_set)
+
+    firefox_set = set(line.strip() for line in open('firefox.txt'))
+
+    print(firefox_set)
+
+
 
 if __name__ == '__main__':
     main()
