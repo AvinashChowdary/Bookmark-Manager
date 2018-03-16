0a2e9bb5abde4339def16d2cd49cd3c77b6b5a3f
Avinash Ravilla <avinashchowdary0801@gmail.com>
2018-03-16 15:18:11 -0700

    aggregating but with duplicates

--- bookmark_manager.py
+++ bookmark_manager.py
@@ -1,5 +1,7 @@
 
 def main():
+
+    result_bkmks = []
     
     safari_set = set(line.strip() for line in open('safari.txt'))
     chrome_set = set(line.strip() for line in open('chrome.txt'))
@@ -8,11 +10,32 @@ def main():
     temp_set = safari_set.union(chrome_set)
     total_set = temp_set.union(firefox_set)
 
-    print(total_set)
-
     total_bkmks = list(total_set)
-    
+    total_bkmks.sort(key = lambda s: len(s))
 
+    for actual in total_bkmks:
+        
+        a_protocol, a_link = separate_protocol_link(actual)
+
+        for repeated in total_bkmks:
+
+            if a_link in repeated:
+                if "http://" in a_protocol:
+                    result_bkmks.append(repeated)
+                    result_bkmks.append(actual)
+                else:
+                    result_bkmks.append(actual)
+                    result_bkmks.append(repeated)
+                
+    print(result_bkmks)
+
+def separate_protocol_link(bkmk):
+    protocol, url = bkmk.split("://")
+    return protocol, url
+
+
+
+    
 
 
 if __name__ == '__main__':
